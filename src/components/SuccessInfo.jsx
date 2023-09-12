97377e047db980c77ad269c9ba9909cdb439a5b8
Petro Matkovskyi <petromatkovskij3021@gmail.com>
2023-09-12 09:32:38 +0300

    SuccessInfo layout

---
+++ src/components/SuccessInfo.jsx
@@ -0,0 +1,20 @@
+export default function SuccessInfo() {
+  return (
+    <>
+      <h3 className="text-primary">Thank you for your order!</h3>
+      <p>
+        <b>Order number is: 188787788</b>
+      </p>
+      <p>
+        You will receive an email confirmation shortly to{' '}
+        <a href="!#">jonathan.smith@gamil.com</a>
+      </p>
+      <p>Estimated delivery Day is</p>
+      <p>
+        <b>Friday 1st April 2016</b>
+      </p>
+
+      <p className="text-primary text-underline">Print Recipe</p>
+    </>
+  );
+}
